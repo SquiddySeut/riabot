923c4b747860445d880d7fbfb1e5450f367b0e76
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-06-23 15:55:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,3 @@
-/*const Discord = require('discord.js')
-const client = new Discord.Client();
-
-client.on('ready' () => {
-    console.log('I am ready");
-});
-
-client.on('message', message => {
-    if (message.content === "ping') {
-        message.reply('pong');
-    }
-});
-
-//keep the thing below
-client.login(process.env.BOT_TOKEN);
-//DYwMTc4OTAyNDg1MDQxMTcy.DhBBQg.cIOcSoPyLxK63pP18sF5sUHGwsM bot token
-*/
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
